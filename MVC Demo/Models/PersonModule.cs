737f7034ccc0e83f180059ab48a61b711f479cc1
Maurice Cooper <jc70240@gmail.com>
2020-05-12 04:28:35 -0500

    Add project files.

---
+++ MVC Demo/Models/PersonModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo.Models
+{
+    public class PersonModule
+    {
+        public string firstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool isAlive { get; set; } = true; // default to true
+
+    }
+}
